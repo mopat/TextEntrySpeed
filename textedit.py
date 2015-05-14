13ccc7d7e66ad44d472f09e4435a004fc957f703
mopat <morczinietz@gmail.com>
2015-05-14 18:02:50 +0200

    text change event; timediffernce in ms between the last two keypresses

--- textedit.py
+++ textedit.py
@@ -1,12 +1,15 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import QObject, pyqtSignal
+import datetime
 import re
 
 class SuperText(QtWidgets.QTextEdit):
- 
+
     def __init__(self, example_text):
         super(SuperText, self).__init__()
         self.numbers=[]
@@ -16,21 +19,43 @@ class SuperText(QtWidgets.QTextEdit):
         self.generate_template()
         self.render_template()
         self.initUI()
-        
-    def initUI(self):      
+
+        keyPressedTimestamps = []
+        self.keyPressedTimestamps = keyPressedTimestamps
+        self.keyPressedTimestamps.append(datetime.datetime.now())
+        # connect signal to slot
+        self.textChanged.connect(self.on_text_changed)
+
+
+    def initUI(self):
         self.setGeometry(0, 0, 400, 400)
-        self.setWindowTitle('SuperText')
+        self.setWindowTitle('TextEntrySpeed')
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.setMouseTracking(True)
         self.show()
-        
+
+    def timestamp(self):
+        return QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate)
+
+    def on_text_changed(self):
+        # Number_of_keystroke / time_in_second * 60 * percentages_of_accurate_word
+        self.keyPressedTimestamps.append(datetime.datetime.now())
+        keyPressedArrayLength = len(self.keyPressedTimestamps)
+        timeDifference = self.keyPressedTimestamps[keyPressedArrayLength-1] - self.keyPressedTimestamps[keyPressedArrayLength-2]
+        timeDifferenceInMs = timeDifference.seconds*1000 + timeDifference.microseconds/1000
+        print(timeDifferenceInMs)
+
+    def change_value(self, val_id, amount):
+        self.numbers[int(str(val_id))] += amount / 120
+        self.render_template()
+
     def render_template(self):
         cur = self.textCursor()
-        doc = self.template_doc 
+        doc = self.template_doc
         for num_id, num in enumerate(self.numbers):
             doc = doc.replace('$' + str(num_id) + '$', '%d' % (num))
         self.setHtml(doc)
-        self.setTextCursor(cur) 
+        self.setTextCursor(cur)
 
     def generate_template(self):
         content = str(self.toPlainText())
@@ -46,7 +71,7 @@ class SuperText(QtWidgets.QTextEdit):
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    super_text = SuperText("An 123 Tagen kamen 1342 Personen.")
+    super_text = SuperText("")
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
